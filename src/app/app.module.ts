02ca93bc59249d55842eb3ef0aac2dc0061b1a79
manabotcard <manabot.cards@gmail.com>
2021-11-21 17:55:13 +0700

    add navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavComponent } from './nav/nav.component';
+import { CardsDatabaseComponent } from './cards-database/cards-database.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    CardsDatabaseComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/cards-database/cards-database.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cards-database',
+  templateUrl: './cards-database.component.html',
+  styleUrls: ['./cards-database.component.scss']
+})
+export class CardsDatabaseComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
